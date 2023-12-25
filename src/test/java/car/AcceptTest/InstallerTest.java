4bfef8b62f92ae76a12a9258d3c10c4a17f6659d
IbtihajAssi <145280279+IbtihajAssi@users.noreply.github.com>
2023-12-25 12:28:36 +0200

    Update InstallerTest.java

--- src/test/java/car/AcceptTest/InstallerTest.java
+++ src/test/java/car/AcceptTest/InstallerTest.java
@@ -1,6 +1,7 @@
 package car.AcceptTest;
 
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 import car.Installer;
@@ -13,13 +14,16 @@ public class InstallerTest {
 	int id;
 	String day;
 	String name;
+	String email;
+	String iddd;
 	@Given("installer was log in")
 	public void installerWasLogIn() {
 		installer=new Installer();  
 	}
 
 	@When("installer with email {string} want to view his installation request")
-	public void installerWithEmailWantToViewHisInstallationRequest(String email) {
+	public void installerWithEmailWantToViewHisInstallationRequest(String emaill) {
+		email=emaill;
 	   id=installer.getInstallerId(email);
 	   name=installer.getInstallerName(id);
 	   installer.setTest(true);
@@ -27,6 +31,8 @@ public class InstallerTest {
 
 	@Then("installer can view his installation request")
 	public void installerCanViewHisInstallationRequest() {
+		assertNotNull(installer.getInstallerId(email));
+		assertNotNull(installer.getInstallerName(id));
 		assertTrue(installer.viewInstallationReq(name));		
 	}
     
@@ -50,14 +56,17 @@ public void allInstallerWillPrint() {
 
 @When("installer want to make installation request with id {string} done")
 public void installerWantToMakeInstallationRequestWithIdDone(String idd) {
+	iddd=idd;
   day=installer.getInstallationDay(Integer.parseInt(idd));
-  installer.editDay(day, Integer.parseInt(idd), false);
-  this.id=Integer.parseInt(idd);
-}
+  installer.editDay(day, Integer.parseInt(iddd), false);
+  this.id=Integer.parseInt(iddd);
+} 
 
 @Then("installation request done")
 public void installationRequestDone() {
 	installer.setTest(true);
+	assertNotNull(installer.getInstallationDay(Integer.parseInt(iddd)));
+	assertFalse(installer.editDay(day, Integer.parseInt(iddd), false));
 	assertFalse(installer.removeInstallation(id));  
 }	
 }
