a04567ed8b757a01ae188a7d6225b6e8c54549cf
IbtihajAssi <145280279+IbtihajAssi@users.noreply.github.com>
2023-12-26 15:06:24 +0200

    Update Product

--- src/main/java/car/Product.java
+++ src/main/java/car/Product.java
@@ -3,7 +3,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.SQLException; 
 import java.util.*;
 import java.util.logging.Logger;
 public class Product {
